a706c1432ddf66ebbaa7f754105183e01dba0508
e.varvarov <herenvaro@gmail.com>
2022-07-03 02:14:57 +0300

    move to single rep

--- server.js
+++ server.js
@@ -13,18 +13,13 @@ const port = process.env.PORT || 3020;
 
 var app = express();
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -40,7 +35,11 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
 });
 
 app.listen(port, () => {
